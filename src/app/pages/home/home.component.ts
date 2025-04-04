2aed3c6ae32274e7151b6461c7e45ed66e5e794c
Buddika Kasun <buddikakasun80@gmail.com>
2025-04-04 12:48:15 +0530

    Add nav bar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  router = inject(Router);
+
+  onLogout = () => {
+    localStorage.removeItem('user');
+    this.router.navigate(['/login']);
+  };
+
+  checkLogin = () => {
+     if (!!localStorage.getItem('user')) {
+       return true;
+     }
+     else {
+      this.router.navigate(['/login']);
+      return false;
+     }
+  };
 
 }
